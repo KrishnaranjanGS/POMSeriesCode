eb2456b6553d100facd9fb42b2ffa92461f673e7
KrishnaranjanGS <Krishnaranjan.Guddeh@openlane.com>
2025-12-10 10:23:46 +0530

    updated cart page class, added cartTest test class

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,5 +1,14 @@
 package com.qa.opencart.pages;
 
-public class CartPage {
+import org.openqa.selenium.WebDriver;
 
+import com.qa.opencart.utils.ElementUtil;
+import com.qa.opencart.utils.WaitUtil;
+
+public class CartPage {
+	
+	private WebDriver driver;
+	private ElementUtil eleUtil;
+	private WaitUtil waitUtil;
+	
 }

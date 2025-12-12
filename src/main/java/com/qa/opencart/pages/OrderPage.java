c7a7a9c06dc6538e781910b3aafd887bdd7e3bef
KrishnaranjanGS <krishnaranjan.gs@gmail.com>
2025-12-12 14:22:30 +0530

    added locator and method in OrderPage.java

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,6 +1,12 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage {
 	
+	By price = By.id("price");
 	
+	public void getPrice() {
+		System.out.println("get price");
+	}
 }

66caf7a8a0f1d12bb0e1408675aa739ce0d01d0a
KrishnaranjanGS <krishnaranjan.gs@gmail.com>
2026-02-17 17:22:54 +0530

    cart page and order page changes

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -11,4 +11,5 @@ public class CartPage {
 	private ElementUtil eleUtil;
 	private WaitUtil waitUtil;
 	
+	
 }
--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,10 +1,24 @@
 package com.qa.opencart.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.qa.opencart.utils.ElementUtil;
+import com.qa.opencart.utils.WaitUtil;
 
 public class OrderPage {
 	
+	private WebDriver driver;
+	private ElementUtil eleUtil;
+	private WaitUtil waitUtil;
+	
 	By price = By.id("price");
+	
+	public OrderPage(WebDriver driver) {
+		this.driver = driver;
+		eleUtil = new ElementUtil(driver);
+		waitUtil = new WaitUtil(driver);
+	}
 	
 	public void getPrice() {
 		System.out.println("get price");
